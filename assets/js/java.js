807b281444ccd774d72d0e6c53d080613dfd0e3c
Fire Dragon <daniwhitlock4@gmail.com>
2020-11-22 22:01:24 -0700

    ability to switch questions

--- assets/js/java.js
+++ assets/js/java.js
@@ -4,39 +4,60 @@ var QuizEl = document.querySelector("#quiz");
 var startEl = document.querySelector("#start");
 var timerScore = document.querySelector("#timer-score");
 
-var quizScore =60; //start score/timer at 60 seconds
-var startButtonEl= document.createElement("button"); //start quiz button
-var questionCounter =0;
+var quizScore = 60; //start score/timer at 60 seconds
+var startButtonEl = document.createElement("button"); //start quiz button
+var questionCounter = 0;
 
 var questions = [
-    {q: "Commonly used data types DO NOT include",
-    a: "3. alerts", 
-    c: [ "1. strings","2. booleans","3. alerts","4. numbers"] 
+    {
+        question: "Commonly used data types DO NOT include",
+        answer: [
+            { text: "1. strings", correct: false },
+            { text: "2. booleans", correct: false },
+            { text: "3. alerts", correct: true },
+            { text: "4. numbers", correct: false }
+        ]
     },
-    
-    {q: "The condition in an if/else statement is enclosed with __________.", 
-    a: "3. parenthesis",
-    c: ["1. quotes", "2. curly brackets","3. parenthesis","4. square brackets"]
+    {
+        question: "The condition in an if/else statement is enclosed with __________.",
+        answer: [
+            { text: "1. quotes", correct: false },
+            { text: "2. curly brackets", correct: false },
+            { text: "3. parenthesis", correct: true },
+            { text: "4. square brackets", correct: false }
+        ]
     },
-    
-    {q: "Arrays in JavaScript can be used to store __________. ",
-    a: "4. all of the above",
-    c: ["1. numbers and strings","2. other arrays","3. booleans","4. all of the above"]
+    {
+        question: "Arrays in JavaScript can be used to store __________. ",
+        answer: [
+            { text: "1. numbers and strings", correct: false },
+            { text: "2. other arrays", correct: false },
+            { text: "3. booleans", correct: false },
+            { text: "4. all of the above", correct: true }
+        ]
     },
-    
-    {q: "String values must be enclosed within __________ when being assigned to variables.", 
-    a: "3. quotes",
-    c: ["1. commas","2. curly brackets","3. quotes","4. parenthesis"]
+    {
+        question: "String values must be enclosed within __________ when being assigned to variables.",
+        answer: [
+            { text: "1. commas", correct: false },
+            { text: "2. curly brackets", correct: false },
+            { text: "3. quotes", correct: true },
+            { text: "4. parenthesis", correct: false }
+        ]
     },
-   
-    {q: "A very useful tool used during development and debugging for printing content to the debugger is: ",
-    a: "4. console.log",
-    c: ["1. JavaScript","2. terminal/bash","3. for loops","4. console.log"] 
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        answer: [
+            { text: "1. JavaScript", correct: false },
+            { text: "2. terminal/bash", correct: false },
+            { text: "3. for loops", correct: false },
+            { text: "4. console.log", correct: true }
+        ]
     }
 ];
 
 //Start button starts quiz
-var WelcomeQuiz = function() {
+var WelcomeQuiz = function () {
     //header info
     var h1El = document.createElement("h1"); //create header for Coding Quiz Challenge
     h1El.textContent = "Coding Quiz Challenge";
@@ -50,18 +71,18 @@ var WelcomeQuiz = function() {
     QuizEl.appendChild(textEl); //make description appear
 
     //button info
-    var buttonCenterEl = document.createElement("div"); 
+    var buttonCenterEl = document.createElement("div");
     var buttonContainerEl = document.createElement("div");
     startButtonEl.textContent = " Start Quiz";
     startButtonEl.className = "button-style";
     buttonCenterEl.className = "button-center";
-    buttonContainerEl.className ="button-container";
+    buttonContainerEl.className = "button-container";
     buttonCenterEl.appendChild(startButtonEl);
     buttonContainerEl.appendChild(buttonCenterEl);
     QuizEl.appendChild(buttonContainerEl);
-    
+
     // Hide information from welcomequiz when start button is clicked
-    if (startButtonEl.addEventListener("click", function() {
+    if (startButtonEl.addEventListener("click", function () {
         h1El.className = "hide";
         textEl.className = "hide";
         startButtonEl.className = "hide";
@@ -69,69 +90,84 @@ var WelcomeQuiz = function() {
 };
 
 //function to go through and display answers as buttons
-var displayQuestionsAnswers =function () {
-
+var displayQuestions = function () {
+    console.log("hello");
     //Display questions, and then each question's specific answer choices
-    if (questionCounter <  questions.length) {
+    if (questionCounter <= questions.length) {
         var questionPage = document.createElement("div");
         var questionHeader = document.createElement("div");
-        var question =document.createElement("h1");
+        var question = document.createElement("h1");
         question.className = "h1-question";
-        question.textContent =questions[questionCounter].q;
+        question.textContent = questions[questionCounter].question;
         questionHeader.appendChild(question);
         questionPage.appendChild(questionHeader);
         QuizEl.appendChild(questionPage);
 
-        for (var j =0; j < questions[i].c.length; j++) {
-            // var choicesContainer = document.createElement("div")
-            var buttonContainerEl = document.createElement("div");
-            buttonContainerEl.className ="button-container";
-            var choicesButton= document.createElement("button");
-            choicesButton.className = "button-style";
-            choicesButton.textContent = questions[i].c[j];
-            buttonContainerEl.appendChild(choicesButton);
-            questionPage.appendChild(buttonContainerEl);
+                
+        function displayAnswerChoices() {
+            for (var j = 0; j < questions[questionsCounter].answer.length; j++) {
+                var buttonContainerEl = document.createElement("div");
+                buttonContainerEl.className = "button-container";
+                var choicesButton = document.createElement("button");
+                choicesButton.className = "button-style";
+                choicesButton.textContent = questions[questionCounter].answer.text[j];
+                buttonContainerEl.appendChild(choicesButton);
+                questionPage.appendChild(buttonContainerEl);
+                console.log(choicesButton);
+
+                choicesButton.addEventListener ("click", function () {
+
+                    if (i === 0 || i === 1 || i === 3) {
+                        if (questions[questionCounter].c === questions[questionCounter].a) { //need to set if button of choices is clicked
+                            var correctAnswerDisplay = document.createElement("div");
+                            correctAnswerDisplay.className = "answer";
+                            correctAnswerDisplay.textContent = "RIGHT!";
+                            QuizEl.appendChild(correctAnswerDisplay);
+                        }
+                        else {
+                        var incorrectAnswerDisplay = document.createElement("div");
+                        incorrectAnswerDisplay.className = "answer";
+                        incorrectAnswerDisplay.textContent = "WRONG!"
+                        QuizEl.appendChild(incorrectAnswerDisplay);
+                        quizScore = quizScore - 10;
+                    }
+                }
+            }
         }
+        //create next button
+        var nextButtonContainer = document.createElement("div");
+        var nextButton = document.createElement("button");
+        nextButtonContainer.className = "button-right";
+        nextButton.className = "button-change-color";
+        nextButton.textContent = "NEXT";
+        nextButtonContainer.appendChild(nextButton);
+        QuizEl.appendChild(nextButtonContainer);
+
+        nextButton.addEventListener("click", function () {
+            console.log("i clicked the button");
+            hideText(question, nextButton);
+            questionCounter++;
+            displayQuestions();
+        });
     }
-    //when ANY BUTTON IS HOVERED OVER, call hover function 
-    //ChangeColorHover();
-    
-    //this-- reads just the button that was clicked
-        //When choicesButton is clicked
 
-    choicesButton.addEventListener("click", CheckAnswers);
-            
-            // );
-            // check to see if answer is correct
-                //if yes, continue on to next question
-                    //have text pop up below and say Correct
-                //if no, subtract 10 from quizScore
-                    //have text pop up below and say Wrong
-
-        //move to next question
-                        
-            
-    
-    //if timer <= 0 || all questions are answered then game over
-    
+};
+//hide text
+var hideText = function (question, nextButton) {
+    question.className = "hide";
+    nextButton.className = "hide";
 };
 
-var ChangeColorHover = function() {
-    choicesButton.addEventListener("mouseover", function (event) {
-        event.target.className ="button-hover";
-    })
-    // change background color to third-color & use removeAttribute to get rid of style when no longer hovering
-};
 
 //Timer Function 
 var ScoreTracker = function () {
-    var timeInterval = setInterval(function() {
-        if (quizScore > 0 ) {
+    var timeInterval = setInterval(function () {
+        if (quizScore > 0) {
             timerScore.className = "score-style";
             timerScore.textContent = "Score: " + quizScore;
             quizScore--; //decrement by 1 every second or pass through setInterval function 
         }
-        else  if (quizScore === 0) {
+        else if (quizScore === 0) {
             GameOver();
         }
     }, 1000);
@@ -140,20 +176,18 @@ var ScoreTracker = function () {
 var startQuiz = function () {
     //when startbutton is clicked, timer starts 
     ScoreTracker();
-    
-    //Go through array of questions and choices
-    displayQuestionsAnswers();
 
-   
+    //Go through array of questions and choices
+    displayQuestions();
 };
 
-var GameOver = function() {
+var GameOver = function () {
     // alert("The game is over!");
 
     //When game over
-        //save my initials and score
-            //TO DO: create an array to save all the information- initials and score and potentially an id to specify which of the scores- use push to get in
-    
+    //save my initials and score
+    //TO DO: create an array to save all the information- initials and score and potentially an id to specify which of the scores- use push to get in
+
 };
 
 WelcomeQuiz();
